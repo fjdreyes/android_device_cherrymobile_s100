6d4c912e2a4dbd3633a2e10f981224ec7bba7894
Franz Delos Reyes <fjdreyes@gmail.com>
2015-01-03 23:30:42 +0000

    mkvendor.sh: Initial commit of s100

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := s100
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/cherrymobile/s100/device_s100.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := s100
+PRODUCT_NAME := cm_s100
+PRODUCT_BRAND := cherrymobile
+PRODUCT_MODEL := s100
+PRODUCT_MANUFACTURER := cherrymobile
